26545e6734dc9d781b813d5c6c3796296dc08150
Rajan Ravisaheb <rajanroy220@gmail.com>
2023-05-08 10:45:22 +0530

    class component added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="App">
-         <h1>hiiii</h1>
+         
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from './App';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-   
+    
     <Navbar />
     <div className="container">
     <Form heading = "this is form" />
